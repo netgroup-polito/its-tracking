6b2b0101137bddd5c559138a55eee4d2d3612fd3
EugenioGallea <gallea6@gmail.com>
2018-12-03 16:42:29 +0100

    Added subfolder with Maven project.

---
+++ server/src/main/java/its_tracking/server/App.java
@@ -0,0 +1,12 @@
+package its_tracking.server;
+
+import javax.ws.rs.Path;
+
+@Path("")
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
